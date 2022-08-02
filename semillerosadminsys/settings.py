95759536a9fef083fbb5c1e6445712f93c25a518
Edwin Blanco <edwinblanco316@gmail.com>
2022-08-01 22:32:43 -0500

    cambio version django 4.0.6

--- semillerosadminsys/settings.py
+++ semillerosadminsys/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['http://semillerosadminsys-env.eba-fj8ggskj.us-west-2.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ['http://semillerosadminsys-env.eba-nges2hxp.us-west-2.elasticbeanstalk.com/']
 
 # Application definition
 
